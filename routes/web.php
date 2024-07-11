6254292f252e54681db83051a5f94cfbbdc48fc9
IhsanSonz <mnurihsanmutaqin66@gmail.com>
2024-07-11 17:35:29 +0700

    feat: adding middlewares on route

--- routes/web.php
+++ routes/web.php
@@ -14,20 +14,20 @@ use App\Http\Controllers;
 |
 */
 
-Route::get('/', [Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
 
 Route::get('/login', [Controllers\AuthController::class, 'login'])->name('login');
 Route::post('/authenticate', [Controllers\AuthController::class, 'authenticate'])->name('authenticate');
 Route::get('/logout', [Controllers\AuthController::class, 'logout'])->name('logout');
 
-Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
+Route::group(['prefix' => 'user', 'as' => 'user.', 'middleware' => 'auth'], function () {
     Route::get('/', [Controllers\UserController::class, 'index'])->name('index');
     Route::get('/create', [Controllers\UserController::class, 'create'])->name('create');
     Route::post('/store', [Controllers\UserController::class, 'store'])->name('store');
     Route::post('/destroy/{id}', [Controllers\UserController::class, 'destroy'])->name('destroy');
 });
 
-Route::group(['prefix' => 'certificate', 'as' => 'certificate.'], function () {
+Route::group(['prefix' => 'certificate', 'as' => 'certificate.', 'middleware' => 'auth'], function () {
     Route::get('/', [Controllers\CertificateController::class, 'index'])->name('index');
     Route::get('/create', [Controllers\CertificateController::class, 'create'])->name('create');
     Route::post('/store', [Controllers\CertificateController::class, 'store'])->name('store');
@@ -37,7 +37,7 @@ Route::group(['prefix' => 'certificate', 'as' => 'certificate.'], function () {
 
 });
 
-Route::group(['prefix' => 'participant', 'as' => 'participant.'], function () {
+Route::group(['prefix' => 'participant', 'as' => 'participant.', 'middleware' => 'auth'], function () {
     Route::get('/', [Controllers\ParticipantController::class, 'index'])->name('index');
     Route::get('/create', [Controllers\ParticipantController::class, 'create'])->name('create');
     Route::post('/store', [Controllers\ParticipantController::class, 'store'])->name('store');
@@ -47,7 +47,7 @@ Route::group(['prefix' => 'participant', 'as' => 'participant.'], function () {
 });
 
 
-Route::group(['prefix' => 'assesment', 'as' => 'assesment.'], function () {
+Route::group(['prefix' => 'assesment', 'as' => 'assesment.', 'middleware' => 'auth'], function () {
     Route::get('/', [Controllers\AssesmentController::class, 'index'])->name('index');
     Route::get('/show/{id}', [Controllers\AssesmentController::class, 'show'])->name('show');
     Route::get('/create', [Controllers\AssesmentController::class, 'create'])->name('create');
